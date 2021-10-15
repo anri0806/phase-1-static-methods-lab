a1af988c46f224d322c724a21097fae6d06c801b
Anri <anri0806@hotmail.co.jp>
2021-10-14 23:46:06 -0400

    commit tree 1c3783d1d4183de5973352fcbf65075b9f2b0963

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    let result = []
+    let splittedWords = string.split(' ')
+    for(let i = 0; i < splittedWords.length; i++)
+      if(i === 0) {
+        result.push(this.capitalize(splittedWords[i]))
+      } else {
+        if(exceptions.includes(splittedWords[i])) {
+          result.push(splittedWords[i])
+        } else {
+          result.push(this.capitalize(splittedWords[i]))
+        }
+      }
+    
+    return result.join(' ')
+  }
 }
+
